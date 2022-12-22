517086b0a364775ee6309433941a3c40af80bd2d
Arno Simons <arno.simons@gmail.com>
2022-12-23 00:05:17 +0100

    Update scratchbook.js

--- static/js/scratchbook.js
+++ static/js/scratchbook.js
@@ -3,9 +3,11 @@ $(document).ready(function () {
   document.getElementById("notationButton").click();
   document.getElementById("elementsButton").click();
   document.getElementById("operatorsButton").click();
+  document.getElementById("explainerButton").click();
 
-  var formulaInput = document.getElementById("formulaInput");
-  var formulaInput = document.getElementById("formulaInput");
+  let formulaInput = document.getElementById("formulaInput");
+  let formulaButton = document.getElementById("formulaButton");
+//   let surpriseButton = document.getElementById("surpriseButton");
 
   let tooltipMessage = "Click to audio-visualize!"
 
@@ -13,6 +15,69 @@ $(document).ready(function () {
     trigger: "hover",
     container: 'body',
     title: tooltipMessage
+  });
+
+  // Function for surprise_button
+  $("#surpriseButton").click(function(){
+    let scratches = [
+      'aquaman',
+      'autobahn',
+      'babyorbit',
+      'boomerang',
+      'boomerang_roll',
+      'brbhippopotamus',
+      'chirp',
+      'chirpboomerang',
+      'chirpflare1',
+      'chirpflare2',
+      'chirpogflare',
+      'chirpogflare_roll',
+      'clovertears',
+      'delete',
+      'diceorbit',
+      'drills',
+      'enneagon',
+      'enneagon_roll',
+      'flareorbit1',
+      'flareorbit2',
+      'flareorbit3',
+      'hendecagon',
+      'hendecagon_roll',
+      'hippopotamus',
+      'hippopotamus_roll',
+      'internet',
+      'kermit',
+      'mflare1',
+      'mflare2',
+      'military',
+      'ogflare',
+      'prizm',
+      'prizm_roll',
+      'rawhippopotamus',
+      'royalline',
+      'scribble',
+      'seesaw',
+      'slice',
+      'slicecombo1',
+      'slicecombo2',
+      'scribbleflare1',
+      'scribbleflare2',
+      'spairflare',
+      'squareflare',
+      'stab',
+      'stabcrab',
+      'stabcrab_roll',
+      'swingflare',
+      'swirl',
+      'tazer1',
+      'tazer1_roll',
+      'tazer2',
+      'tazer2_roll',
+      'turnaroundtransform',
+      'xenon',
+    ]
+    formulaInput.value = scratches[Math.floor(Math.random() * scratches.length)];
+    formulaButton.click();
   });
 
   // Formula
@@ -30,30 +95,30 @@ $(document).ready(function () {
         // clear elements
         $( ".home-scratch__message" ).empty();
         $( ".home-scratch__ttm" ).empty();
-        $( ".home-scratch__audio" ).empty(); 
-        $( ".home-scratch__info" ).empty(); 
+        $( ".home-scratch__audio" ).empty();
+        $( ".home-scratch__info" ).empty();
         if (data["isWorking"]) {
           let processedFormula = data["processedFormula"].replaceAll('/', '$')
           // create TTM element
           $(".home-scratch__ttm").append(`
-            <div class="row overflow-auto">
+            <div class="row">
               <picture>
-                <source 
-                  srcset="/formulas/${processedFormula}/scratchbook_ttm" 
+                <source
+                  srcset="/formulas/${processedFormula}/scratchbook_ttm"
                   type="image/svg+xml">
-                <img class="img" src="alt.png" alt="TTM Plot as svg" />
+                <img class="img-fluid" src="alt.png" alt="TTM Plot as svg" />
               </picture>
             </div>
           `)
-          // create audio element 
+          // create audio element
           $(".home-scratch__audio").append(`
-          <audio controls>
-            <source 
-              src="/formulas/${processedFormula}/scratchbook_audio" 
+          <audio controls loop>
+            <source
+              src="/formulas/${processedFormula}/scratchbook_audio"
               type="audio/wav">
             Your browser does not support the audio element.
           </audio>
-          `)              
+          `)
           // create info elements
           // analysis
           let groups = ['Elements', 'Combos']
@@ -63,15 +128,15 @@ $(document).ready(function () {
               let figures = []
               for (let member of data["info"][group.toLowerCase()]){
                 figures.push(`
-                  <figure 
-                      class="figure tooltip-figure" 
+                  <figure
+                      class="figure tooltip-figure"
                       onclick="document.getElementById('formulaInput').value = '${member}'; formulaButton.click();"
                     >
-                    <img src="/formulas/${member}/preview" class="figure-img img-fluid rounded" alt="preview">
+                    <img src="/formulas/${member}/preview" class="figure-img preview rounded" alt="preview">
                     <figcaption class="figure-caption">
                       ${member}
                     </figcaption>
-                  </figure>  
+                  </figure>
                 `)
               }
               let groupHTML = `
@@ -85,14 +150,14 @@ $(document).ready(function () {
             }
           }
           $(".home-scratch__info").append(`
-            <div class="col">
+            <div class="col-sm-6">
               <div class="card border mb-3" style="min-width: 9rem;">
                 <div class="card-header">
-                  <a 
-                    class="btn cardHeader" 
-                    data-bs-toggle="collapse" 
+                  <a
+                    class="btn fw-semibold"
+                    data-bs-toggle="collapse"
                     aria-expanded="true"
-                    href=".home-scratch__info-analysis" 
+                    href=".home-scratch__info-analysis"
                     style="width: 100%">
                     &#128270; Analysis
                   </a>
@@ -104,7 +169,7 @@ $(document).ready(function () {
                     Your scratch has ${data["info"]["F"]} ${data["info"]["F"] == 1 ? 'click' : 'clicks'} and makes ${data["info"]["sounds"]} ${data["info"]["sounds"] == 1 ? 'sound' : 'sounds'}
                   </p>
                   <p class="card-text">
-                    It contains ${data["info"]["variations"] == 1 ? '(variations of) ' : ''} the following generic components:                      
+                    It contains ${data["info"]["variations"] == 1 ? '(variations of) ' : ''} the following generic components:
                   </p>
                   <div class="card-group">
                     ${groupsHTML.map(i => i).join(" ")}
@@ -128,28 +193,28 @@ $(document).ready(function () {
               )
               items.push(`
                 <div class="carousel-item ${index == 0 ? 'active' : '' } ratio ratio-4x3">
-                  <iframe 
-                    src="https://www.youtube.com/embed/${ youtubeID }" 
-                    title="YouTube video player" 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  <iframe
+                    src="https://www.youtube.com/embed/${ youtubeID }"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
-                  </iframe> 
+                  </iframe>
                 </div>
               `)
             }
             $(".home-scratch__info").append(`
-              <div class="col">
+              <div class="col-sm-6">
                 <div class="card border mb-3" style="min-width: 9rem;">
                   <div class="card-header">
-                    <a 
-                      class="btn cardHeader" 
-                      data-bs-toggle="collapse" 
+                    <a
+                      class="btn fw-semibold"
+                      data-bs-toggle="collapse"
                       aria-expanded="true"
-                      href=".home-scratch__info-tutorials" 
+                      href=".home-scratch__info-tutorials"
                       title="Learn how to speak scratch algebra"
                       style="width: 100%">
-                      &#128250; Related Tutorials            
+                      &#128250; Related Tutorials
                     </a>
                   </div>
                   <div class="home-scratch__info-tutorials collapse show">
@@ -187,10 +252,10 @@ $(document).ready(function () {
       }
     })
   });
-  
+
   // Library
   ////////////////////////////////////////////////////////////////////////////////////
-  
+
   var table = $('#library').DataTable({
     serverSide: true,
     ajax: "/library/data",
@@ -204,11 +269,12 @@ $(document).ready(function () {
       {data:"libraries", title: "libraries", visible: false}, // 6
     ],
     order: [
-      [ 0, "asc" ], 
+      [ 0, "asc" ],
     ],
     "searching": true,
     "lengthChange": true,
     scrollY: '50vh',
+    scrollX: true,
     scrollCollapse: true,
     fixedHeader: {
       header: true,
@@ -217,11 +283,24 @@ $(document).ready(function () {
       // Tooltips for clicking Previews and Names
       $('#library tbody tr').each(function () {
         var tr = $(this);
-        td1 = tr.find("td:eq(0)");
-        td1.attr('title', tooltipMessage);
-
-        td2 = tr.find("td:eq(1)");
-        td2.attr('title', tooltipMessage);
+        tr.find("td:eq(0)").attr('title', tooltipMessage);
+        tr.find("td:eq(1)").attr('title', tooltipMessage);
+        tr.find("td:eq(2)").attr('title', tooltipMessage);
+        tr.find("td:eq(3)").attr('title', tooltipMessage);
+        tr.find("td:eq(4)").attr('title', tooltipMessage);
+        tr.find("td:eq(5)").attr('title', tooltipMessage);
+        // td0 = tr.find("td:eq(0)");
+        // td0.attr('title', tooltipMessage);
+        // td1 = tr.find("td:eq(1)");
+        // td1.attr('title', tooltipMessage);
+        // td2 = tr.find("td:eq(2)");
+        // td2.attr('title', tooltipMessage);
+        // td3 = tr.find("td:eq(3)");
+        // td3.attr('title', tooltipMessage);
+        // td4 = tr.find("td:eq(4)");
+        // td4.attr('title', tooltipMessage);
+        // td5 = tr.find("td:eq(5)");
+        // td5.attr('title', tooltipMessage);
       });
       /* Style header tooltips */
       $('#library [title]').tooltip({
@@ -232,17 +311,18 @@ $(document).ready(function () {
       $('#library').on('click', 'td', function () {
         var cell = table.cell( this ).data();
         var colindx = table.column( this ).index();
-        if (colindx == 0 || colindx == 1) {
+        if ([0,1,2,3,4,5].includes(colindx)) {
+        // if (colindx == 0 || colindx == 1 || colindx == 2 || colindx == 3 || colindx == 4 || colindx == 5) {
           var row = table.row( this ).data();
           var names = row["Name(s)"];
           names = names.split(",");
           formulaInput.value = names[0];
           formulaButton.click();
-        }; 
+        };
       });
     },
   });
-  // Functions for row-filtering switches  
+  // Functions for row-filtering switches
   function activeLibs() { // return all currently switched libs
     let libraries = [];
     for (let lib of ["CORE", "ELEMENTS", "TEARS", "ORBITS", "COMBOS"]) {
